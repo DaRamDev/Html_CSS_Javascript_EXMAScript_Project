760a79784736959e660f582f81f7be677e1c821c
DARAM <merj4871@naver.com>
2026-09-15 16:10:05 +0900

    React 클래스와 함수형

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyComponent extends Component {
-    render() {
-        return (
-            <div>
-                <h2>클래스 형태의 컴포넌트</h2>
-                <h3>hello {this.props.name}/ {this.props.age}</h3>
-            </div>
-        );
-    }
+  //상태객체
+  state = {
+    value: 0,
+  };
+
+  render() {
+    const { name, age } = this.props;
+    const { value } = this.state;
+    return (
+      <div>
+        <h2>클래스 형태의 컴포넌트</h2>
+        <h3>Hello! {name} / {age}</h3>
+        <p>상태변수 value = {this.state.value}</p>
+        <button onClick={()=>(this.setState({value:this.state.value+1}))}>증가</button>
+      </div>
+    );
+  }
 }
 
 export default MyComponent;
