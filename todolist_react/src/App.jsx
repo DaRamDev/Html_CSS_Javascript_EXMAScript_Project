a3ed9196d618a1ffd6a7eef9bfe694baae45a526
DARAM <merj4871@naver.com>
2026-09-16 11:03:34 +0900

    mycomponent add

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
-import MyComponent from './components/MyComponent'
-import MyComponentFunc from './components/MyComponentFunc'
+import Form from './components/Form'
+import TodoListTemplate from './components/TodoListTemplate'
 
 function App() {
 
   return (
     <>
-      <MyComponent name="클래스형" age={20} />
-      <hr/>
-      <MyComponentFunc  name="함수형" age={30}>
-        <p>함수형의 하위 엘리먼트 P</p>
-      </MyComponentFunc>  
+      <TodoListTemplate form={<Form />}>
+        템플릿 완성
+      </TodoListTemplate>
+
     </>
   )
 }
